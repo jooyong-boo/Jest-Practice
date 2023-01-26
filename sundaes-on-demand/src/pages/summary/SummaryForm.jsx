1a198a7a9814c0ca9eb36503d2075e18f9a0b9c4
jooyong-boo <qnwndyd159@naver.com>
2023-01-26 17:06:40 +0900

    mock service worker setup

--- sundaes-on-demand/src/pages/summary/SummaryForm.jsx
+++ sundaes-on-demand/src/pages/summary/SummaryForm.jsx
@@ -1,9 +1,26 @@
 import { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 
 const SummaryForm = () => {
     const [check, setCheck] = useState(false);
+
+    const popover = (
+        <Popover id="popover-basic">
+            <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+        </Popover>
+    );
+
+    const checkboxLabel = (
+        <span>
+            I agree to
+            <OverlayTrigger placement="right" overlay={popover}>
+                <span style={{ color: "red" }}> Terms and Conditions </span>
+            </OverlayTrigger>
+        </span>
+    );
     return (
         <Form>
             <Form.Group>
@@ -12,9 +29,10 @@ const SummaryForm = () => {
                     id="disable-button-checked"
                     checked={check}
                     onChange={(e) => setCheck(e.target.checked)}
+                    label={checkboxLabel}
                 />
             </Form.Group>
-            <label htmlFor="disable-button-checked">Disable Button</label>
+
             <Button disabled={!check}>버튼</Button>
         </Form>
     );
--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,19 +1,41 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
-test("체크박스 체크 여부에따라 버튼 활성/비활성", () => {
+test("체크박스 체크 여부에따라 버튼 활성/비활성", async () => {
     render(<SummaryForm />);
-
-    const checkbox = screen.getByRole("checkbox", { name: "Disable Button" });
+    const user = userEvent.setup();
+    const checkbox = screen.getByRole("checkbox");
     const button = screen.getByRole("button", { name: "버튼" });
 
     expect(checkbox).not.toBeChecked(); // 체크박스 비활성화 여부 체크
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(checkbox).toBeChecked();
     expect(button).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(checkbox).not.toBeChecked();
     expect(button).toBeDisabled();
+});
+
+test("체크박스 라벨 위에 마우스를 올리면 팝오버", async () => {
+    const user = userEvent.setup();
+    render(<SummaryForm />);
+
+    // 팝오버 안보임
+    const nullPopover = screen.queryByText(
+        /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+    // 체크박스 라벨 위에 마우스 올리면 팝오버 보임
+    const termsAndContidions = screen.getByText(/terms and conditions/i);
+    await user.hover(termsAndContidions);
+    const popover = screen.getByText(
+        /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+    // 라벨 위를 벗어나면 팝오버 사라짐
+    await user.unhover(termsAndContidions);
+    expect(popover).not.toBeInTheDocument();
 });
