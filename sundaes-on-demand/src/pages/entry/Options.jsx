48715f9590195530cb60298a8fe3a9c6031a7150
jooyong-boo <qnwndyd159@naver.com>
2023-01-26 19:43:31 +0900

    add topping test

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
 import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
 import Row from "react-bootstrap/Row";
 
 export default function Options({ optionType }) {
@@ -16,7 +17,7 @@ export default function Options({ optionType }) {
             });
     }, [optionType]);
 
-    const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+    const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
     const optionItems = items.map((item) => (
         <ItemComponent
--- sundaes-on-demand/src/pages/entry/test/Options.test.jsx
+++ sundaes-on-demand/src/pages/entry/test/Options.test.jsx
@@ -12,3 +12,18 @@ test("displays image for each scoop option from server", async () => {
     const altText = scoopImages.map((ele) => ele.alt);
     expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("displays image for each toppings option from server", async () => {
+    render(<Options optionType="toppings" />);
+    const toppingImages = await screen.findAllByRole("img", {
+        name: /topping$/i,
+    });
+    expect(toppingImages).toHaveLength(3);
+
+    const altText = toppingImages.map((ele) => ele.alt);
+    expect(altText).toEqual([
+        "Cherries topping",
+        "M&Ms topping",
+        "Hot fudge topping",
+    ]);
+});
