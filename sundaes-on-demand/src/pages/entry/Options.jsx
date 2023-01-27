e03b115c32187b12af56799a0baf2a31e847997b
jooyong-boo <qnwndyd159@naver.com>
2023-01-27 17:37:22 +0900

    jest test for msw

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -4,18 +4,24 @@ import { useEffect } from "react";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 
 export default function Options({ optionType }) {
     const [items, setItems] = useState([]);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         axios
             .get(`http://localhost:3030/${optionType}`)
             .then((res) => setItems(res.data))
             .catch((err) => {
-                // Todo: error response
+                setError(true);
             });
     }, [optionType]);
+
+    if (error) {
+        return <AlertBanner />;
+    }
 
     const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
